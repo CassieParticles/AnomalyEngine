e60e8d6fd55cf288dc65f3bc853e157a3a47c7fc
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-12-19 05:39:42 +0000

    Added Entity and Component classes

---
+++ Engine/ECS/Component.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "ComponentConcept.h"
+#include "Entity.h"
+#include "refl.hpp"
+
+namespace Engine
+{
+    class Component
+    {
+    public:
+        Component();
+        Entity getEntity() const{return entity;}
+    private:
+        Entity entity;
+    };
+
+
+    class TestComponent: public Component
+    {
+    public:
+        TestComponent();
+        int data;
+    private:
+    };
+}
+
+REFL_AUTO(type(Engine::Component))
+REFL_AUTO(type(Engine::TestComponent,bases<Engine::Component>))
---
+++ Engine/ECS/Entity.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "ComponentConcept.h"
+
+namespace Engine
+{
+    class Entity
+    {
+    public:
+        operator EntityId() const {return entityId;}
+    private:
+        EntityId entityId;
+    };
+}
